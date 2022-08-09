f758694e70885f9bb177a1f735c98fab3873afad
wormjoo <wormjoo@naver.com>
2022-08-09 19:40:47 +0900

    Fix: 포토스토리 등록 API 수정

--- src/app/Story/storyRoute.js
+++ src/app/Story/storyRoute.js
@@ -1,6 +1,8 @@
 module.exports = function (app) {
   const story = require("./storyController");
   const jwtMiddleware = require("../../../config/jwtMiddleware");
+  const imageUploader = require("../Pose/imageUploader");
+
   // 포토스토리 조회 api
   app.get("/filme/story", jwtMiddleware, story.getStory);
 
@@ -11,7 +13,12 @@ module.exports = function (app) {
   app.get("/filme/story/:storyIdx", story.getOneStory);
 
   // 포토스토리 등록
-  app.post("/filme/story", jwtMiddleware, story.postStory);
+  app.post(
+    "/filme/story",
+    jwtMiddleware,
+    imageUploader.single("image"),
+    story.postStory
+  );
 
   // 포토스토리 수정
   app.put("/filme/story/:storyIdx", story.editStory);
