61a7ee484cfa27c29a7f6c6597d834d402659dd4
juy4556 <kws05050@naver.com>
2022-08-17 04:22:54 +0900

    Fix: ModifyBodySizeLimit

--- config/express.js
+++ config/express.js
@@ -7,11 +7,9 @@ module.exports = function () {
 
   app.use(compression());
 
-  app.use(express.json({ limit: 5000000 }));
+  app.use(express.json({ limit: "50mb" }));
 
-  app.use(
-    express.urlencoded({ limit: 5000000, extended: true, parmeterLimit: 50000 })
-  );
+  app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
   app.use(methodOverride());
 
